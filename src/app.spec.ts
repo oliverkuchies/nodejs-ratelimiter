140d148523b5fcdf41a6ca08973a275dbecb8a51
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-03-27 10:43:44 +1100

    Fix rate limiter

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,25 +1,32 @@
 import request from "supertest";
-import { redisClient } from "./data/datasource";
+import datasource, { redisClient } from "./data/datasource";
+import { RateLimit } from "./data/entities/rate-limit";
 
 const rootURL = "http://localhost:3000";
 
 beforeAll(async () => {
   await redisClient.connect();
   await redisClient.flushAll();
+  // Create rate entity
+  await datasource.initialize();
+  const rateEntity = new RateLimit();
+  rateEntity.windowSeconds = 60;
+  rateEntity.requestsAllowed = 5;
+  rateEntity.routeRegex = "/api/hello";
+  rateEntity.group = "default";
+  await rateEntity.save();
 });
 
-it("responds with json", function (done) {
-  const calls = 6;
+it("rate limiter works accordingly", function (done) {
+  request(rootURL).get("/api/hello").expect(200, done);
 
-  for (let i = 0; i < calls; i++) {
-    request(rootURL)
-      .get("/api/hello")
-      .expect("Content-Type", "text/html; charset=utf-8")
-      .expect(200, done);
-  }
+  request(rootURL).get("/api/hello").expect(200, done);
 
-  request(rootURL)
-    .get("/api/hello")
-    .expect("Content-Type", "text/html; charset=utf-8")
-    .expect(200, done);
+  request(rootURL).get("/api/hello").expect(200, done);
+
+  request(rootURL).get("/api/hello").expect(200, done);
+
+  request(rootURL).get("/api/hello").expect(200, done);
+
+  request(rootURL).get("/api/hello").expect(429, done);
 });
